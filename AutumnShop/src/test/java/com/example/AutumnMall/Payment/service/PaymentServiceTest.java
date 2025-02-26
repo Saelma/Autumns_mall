caadbcbe6db1079e4eae93d03d08919cee00f8db
DESKTOP-QHFFTR8\나 <rlarlgus300@gmail.com>
2025-02-27 00:09:27 +0900

    물품 결제 기능의 테스트를 작성
    물품 결제 기능의 검증 메소드 중 impuid가 같을 경우에 테스트 코드를 작성함.
    Controller에서 에러 코드가 일정해져 원하는 에러 코드가 리턴되지 않던 점 수정함

--- AutumnShop/src/test/java/com/example/AutumnMall/Payment/service/PaymentServiceTest.java
+++ AutumnShop/src/test/java/com/example/AutumnMall/Payment/service/PaymentServiceTest.java
@@ -3,39 +3,35 @@ package com.example.AutumnMall.Payment.service;
 import com.example.AutumnMall.Payment.controller.PaymentController;
 import com.example.AutumnMall.Payment.domain.Payment;
 import com.example.AutumnMall.Payment.dto.AddPaymentDto;
-import com.example.AutumnMall.Payment.service.PaymentService;
+import com.example.AutumnMall.exception.BusinessLogicException;
+import com.example.AutumnMall.exception.ExceptionCode;
 import com.example.AutumnMall.security.jwt.util.LoginUserDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.batch.test.context.SpringBatchTest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.List;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@ActiveProfiles("test")
 @Slf4j
-@SpringBatchTest
-@SpringBootTest
 @ExtendWith({MockitoExtension.class, RestDocumentationExtension.class})
 @AutoConfigureRestDocs
 public class PaymentServiceTest {
@@ -53,47 +49,91 @@ public class PaymentServiceTest {
         mockMvc = MockMvcBuilders.standaloneSetup(paymentController)
                 .apply(MockMvcRestDocumentation.documentationConfiguration(restDocumentationContextProvider))
                 .build();
-
     }
 
     @Test
     public void testAddPayment_Success() throws Exception {
-        Long memberId = 1L;
-        Long cartId = 1L;
-        Long orderId = 1L;
+        Long memberId = 31L;
+        Long cartId = 7L;
+        Long orderId = 161L;
         String impuid = "imp53071323";
         List<Integer> quantities = List.of(1, 2);
 
-        // AddPaymentDto 설정
+        // 요청 DTO 설정
         AddPaymentDto addPaymentDto = new AddPaymentDto();
         addPaymentDto.setCartId(cartId);
         addPaymentDto.setOrderId(orderId);
         addPaymentDto.setQuantity(quantities);
         addPaymentDto.setImpuid(impuid);
 
-        // Payment 객체 설정
+        // 응답 Payment 객체 설정
         Payment payment = new Payment();
         payment.setId(1L);
-        payment.setPrice((double) 100L);
-        payment.setImpuid("imp53071323");
+        payment.setPrice(100.0);
+        payment.setImpuid(impuid);
 
         // LoginUserDto 설정
         LoginUserDto loginUserDto = new LoginUserDto();
         loginUserDto.setMemberId(memberId);
 
-        // Mocking: paymentService.addPayment 메서드 호출 시 Payment 객체 반환
+        // Mock 설정
         when(paymentService.addPayment(memberId, cartId, orderId, quantities, impuid))
                 .thenReturn(List.of(payment));
 
-        // 테스트: 결제 추가 엔드포인트 호출
+        // When & Then (테스트 실행 및 검증)
         mockMvc.perform(post("/payment")
+                        .param("memberId", String.valueOf(loginUserDto.getMemberId()))
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(new ObjectMapper().writeValueAsString(addPaymentDto)))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(status().isOk())
                 .andDo(document("add-payment-success"));
 
-        // 검증: 서비스 메서드가 한 번 호출되었는지 확인
+        // 서비스가 1회 호출되었는지 확인
         verify(paymentService, times(1))
                 .addPayment(memberId, cartId, orderId, quantities, impuid);
     }
+
+    @Test
+    public void testAddPayment_FAILED_INVALID_IMPUID() throws Exception {
+        Long memberId = 31L;
+        Long cartId = 7L;
+        Long orderId = 161L;
+        String impuid = "imp266394620868";
+        List<Integer> quantities = List.of(1, 2);
+
+        // 요청 DTO 설정
+        AddPaymentDto addPaymentDto = new AddPaymentDto();
+        addPaymentDto.setCartId(cartId);
+        addPaymentDto.setOrderId(orderId);
+        addPaymentDto.setQuantity(quantities);
+        addPaymentDto.setImpuid(impuid);
+
+        // 응답 Payment 객체 설정
+        Payment payment = new Payment();
+        payment.setId(1L);
+        payment.setPrice(100.0);
+        payment.setImpuid(impuid);
+
+        // LoginUserDto 설정
+        LoginUserDto loginUserDto = new LoginUserDto();
+        loginUserDto.setMemberId(memberId);
+
+        // 잘못된 impUid에 대한 Mock 설정 (PAYMENT_ALREADY_PAID 예외 처리)
+        when(paymentService.addPayment(memberId, cartId, orderId, quantities, impuid))
+                .thenThrow(new BusinessLogicException(ExceptionCode.PAYMENT_ALREADY_PAID));
+
+        // When & Then (테스트 실행 및 검증)
+        mockMvc.perform(post("/payment")
+                        .param("memberId", String.valueOf(loginUserDto.getMemberId()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(addPaymentDto)))
+                .andExpect(status().isBadRequest())  // 400 상태 코드 예상
+                .andExpect(jsonPath("$.message").value("이미 처리된 결제입니다."))
+                .andDo(document("add-payment-invalid-impuid"));
+
+        // 서비스 메서드가 1회 호출되었는지 확인
+        verify(paymentService, times(1)) // 1회 호출되는지 확인
+                .addPayment(memberId, cartId, orderId, quantities, impuid);
+    }
+
 }
